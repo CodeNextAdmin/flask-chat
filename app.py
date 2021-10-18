f127528afeb8c1152c5453bb6bbda60259f79846
Code Next <61989260+CodeNextAdmin@users.noreply.github.com>
2021-10-18 13:29:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 sense = SenseHat()
 
-app.config['SECRET_KEY'] = SOCKET-IO-SECRET #CHANGE TO YOUR OWN
+app.config['SECRET_KEY'] = SOCKET_IO_SECRET #CHANGE TO YOUR OWN
 socketio = SocketIO(app)
 
 def messageReceived(methods=['GET', 'POST']):
